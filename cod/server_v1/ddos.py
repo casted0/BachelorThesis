ff38d3b910da0d4eb1dd9e05b329e8c6213fc143
Pablo Castedo Sanjuan <pablo.castedo@estudiante.uam.es>
2021-04-25 16:01:35 +0200

    El socket no se cierra y el servidor resetea la conexion

---
+++ cod/server_v1/ddos.py
@@ -0,0 +1,22 @@
+import requests
+import threading
+import os
+
+url = "curl 127.0.0.1:8080"
+
+def curl():
+    while True:
+        os.system(url)
+
+threadList = []
+
+for i in range(100):
+    t = threading.Thread(target=curl)
+    t.demon = True
+    threadList.append(t)
+
+for i in range(100):
+    threadList[i].start()
+
+for i in range(100):
+    threadList[i].join()
