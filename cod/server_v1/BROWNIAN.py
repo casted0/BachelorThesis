dbf2dde74b39de02f9c7739d54a80d9edf4b405e
Pablo Castedo Sanjuan <pablo.castedo@estudiante.uam.es>
2021-06-01 18:50:42 +0200

    requests acabadas queda empezar recoleccion y memoria

--- cod/server_v1/BROWNIAN.py
+++ cod/server_v1/BROWNIAN.py
@@ -2,7 +2,7 @@ from math import sqrt
 from scipy.stats import norm
 import numpy as np
 import matplotlib
-from pylab import plot, show, grid, xlabel, ylabel
+from pylab import plot, show, grid, xlabel, ylabel, legend
 from fracdiff import fdiff
 
 
@@ -26,33 +26,44 @@ def brownian(a, n, dt, delta, out=None):
     out += np.expand_dims(a, axis=-1)
 
     return out
+    
+def fracdif_brownian(x):
+    return fdiff(np.absolute(x), n=0.2)
 
 
 if __name__ == "__main__":
 
     # The Wiener process parameter.
-    delta = 4
+    delta = 2
     # Total time.
     T = 24*60
     # Number of steps.
-    N = 500
+    N = 24*60
     # Time step size
     dt = T/N
-    # Number of realizations to generate.
-    m = 2
     # Create an empty array to store the realizations.
-    x = np.empty((m,N+1))
+    x = np.empty((1,N+1))
+    y = np.empty((1,N+1)) 
     # Initial values of x.
     x[:, 0] = 0
+    y[:, 0] = 0
     
     brownian(x[:,0], N, dt, delta, out=x[:,1:])
-    
+    y[0] = x[0]
+    print(x[0])
     t = np.linspace(0.0, N*dt, N+1)
-    fdiff(x[1], n=0.5)
-    for k in range(m):
-        plot(t, x[k])
-        plot(t, x[1])
-    xlabel('t', fontsize=16)
-    ylabel('x', fontsize=16)
+    
+    # En el caso de que el ruido generado sea negativo, no tiene sentido que no haya peticiones o sean negativas
+    # asi que se usan los valores absolutos del ruido generado, que despues son suavizados para el script
+    
+    y = fracdif_brownian(x)
+    
+    print(y[0])
+    
+    plot(t, x[0], "-b", label="Original Array")
+    plot(t, y[0], "-g", label="Fraccional diff Array")
+    legend()
+    xlabel('Time (minutes)', fontsize=16)
+    ylabel('Requests (per Minute)', fontsize=16)
     grid(True)
     show()
--- cod/server_v1/request.py
+++ cod/server_v1/request.py
@@ -1,22 +1,78 @@
 import requests
 import threading
 import os
+import BROWNIAN
+import numpy as np
+import time
 
 url = "curl 127.0.0.1:8080"
 
 def curl():
-    while True:
+    for i in range(5):
         os.system(url)
 
 threadList = []
 
-for i in range(5):
-    t = threading.Thread(target=curl)
-    t.demon = True
-    threadList.append(t)
+    
+# The Wiener process parameter.
+delta = 2
 
-for i in range(5):
-    threadList[i].start()
+# Total time.
+T = 24*60
 
-for i in range(5):
-    threadList[i].join()
+# Number of steps.
+N = 24*60
+
+# Time step size
+dt = T/N
+
+# Create an empty array to store the realizations.
+x = np.empty((1,N+1))
+y = np.empty((1,N+1)) 
+
+# Initial values of x.
+x[:, 0] = 0
+y[:, 0] = 0
+
+BROWNIAN.brownian(x[:,0], N, dt, delta, out=x[:,1:])
+y[0] = x[0]
+t = np.linspace(0.0, N*dt, N+1)
+
+# En el caso de que el ruido generado sea negativo, no tiene sentido que no haya peticiones o sean negativas
+# asi que se usan los valores absolutos del ruido generado, que despues son suavizados para el script
+
+y = BROWNIAN.fracdif_brownian(x)
+
+for i in range(y.size):
+
+
+    print("Minuto: ", i)
+    print("Peticiones: ", int(y[0][i]))
+
+    for j in range(int(y[0][i])*2):
+        t = threading.Thread(target=curl)
+        t.demon = True
+        threadList.append(t)
+        
+    for j in range(int(y[0][i])*2):
+        if not t.is_alive():
+            threadList[j].start()
+        
+    for j in range(int(y[0][i])*2):
+        threadList[j].join()
+        
+    threadList.clear()
+        
+        
+        
+        
+"""
+
+En el array hay 60*24 items, la cantidad de minutos en un dia
+por lo tanto esa es la cantidad de veces que se va a:
+
+1. Crear n*2 hilos (Siendo n el numero del array en la iteracion correspondiente)
+2. Cada hilo va a ejecutar 5 peticiones al servidor
+    - Cada peticion hace una llamada simple que casi no consume tiempo, el resultado estara entre 0*2 y 50*2 a lo sumo *5 = unas 500 peticiones en un minuto
+
+"""
